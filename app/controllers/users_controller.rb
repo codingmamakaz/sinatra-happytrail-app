65c9f5c0084ca08796605baf84f7735deb0594d6
github username <github email address>
2019-01-17 02:07:33 -0500

    difine get signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+    get '/signup' do
+        if !logged_in?
+          erb  :'/users/create_user'
+        else
+          redirect '/trails'
+        end
+    end
 end
