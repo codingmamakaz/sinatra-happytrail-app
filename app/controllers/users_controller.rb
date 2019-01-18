fd5c62a985644844de726fb1f5e2d2a6706f8c05
github username <github email address>
2019-01-17 22:01:43 -0500

    define get /login in user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,37 @@
 class UsersController < ApplicationController
 
     get '/signup' do
-        if !logged_in?
-          erb  :'/users/create_user'
-        else
-          redirect '/trails'
-        end
+        # if !logged_in?
+        #   erb  :'/users/create_user'
+        # else
+        #   redirect '/trails'
+        # end
         erb  :'/users/create_user'
     end
 
+    post '/signup' do 
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+          redirect to '/signup'
+        else
+          @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+          @user.save
+          session[:user_id] = @user.id
+          redirect to '/trails'
+        end
+      end
+
+    get '/login' do 
+        if !logged_in?
+          erb :'users/login'
+        else
+          redirect '/trails'
+        end
+        erb :'users/login'
+    end
 
     
 end
 
 
-#work on sign up form
+
 #work on post "/signup"
-#don't forget to add 'use UsersController' in config.ru
