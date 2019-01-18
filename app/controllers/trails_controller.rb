d5913c3a17ed7d7190e8c8533a47480e0ca050f7
github username <github email address>
2019-01-18 00:57:30 -0500

    change welcome greeting

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -2,6 +2,7 @@ class TrailsController < ApplicationController
 
     get '/trails' do
         if logged_in?
+            @user = User.find_by_id(session[:user_id])
             erb :'trails/trails'
         else
             redirect '/login'
