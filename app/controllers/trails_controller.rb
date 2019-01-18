c39eb6377e2be9fd3ec84f259455a5434fab03eb
github username <github email address>
2019-01-18 15:03:22 -0500

    add post /trails user can view all the trails logged

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -3,6 +3,7 @@ class TrailsController < ApplicationController
     get '/trails' do
         if logged_in?
             @user = User.find_by_id(session[:user_id])
+            @trails = Trail.all.each{|trail|trail.name}
             erb :'trails/trails'
         else
             redirect '/login'
@@ -18,8 +19,11 @@ class TrailsController < ApplicationController
         end
     end
 
-    post 'trails' do
-        binding.pry
+    post '/trails' do
+        # binding.pry
+        @trail = Trail.create(params["trail"])
+        @trails = Trail.all.each{|trail|trail.name}
+        redirect '/trails'
     end
 
 end
