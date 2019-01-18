2e16f83057d449d12e610d61f4f20cba0b7cb02e
github username <github email address>
2019-01-18 00:33:20 -0500

    add logout route and remove home link from /trail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,6 @@ class UsersController < ApplicationController
 
     post '/login' do
         user = User.find_by(:username => params[:username])
-        user.authenticate(params[:password])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
             redirect "/trails"
@@ -38,6 +37,14 @@ class UsersController < ApplicationController
         end
     end
 
+    get '/logout' do
+        if logged_in?
+          session.destroy
+          redirect to '/login'
+        else
+          redirect to '/'
+        end
+    end
     
 end
 
