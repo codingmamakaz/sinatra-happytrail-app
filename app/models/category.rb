440351dfdb3fc123c729c1d4cd936491e5cbbfc0
github username <github email address>
2019-01-20 10:15:16 -0500

    add slug and find-by-slug methods

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,12 @@ class Category < ActiveRecord::Base
     has_many :trail_categories
     has_many :trails, through: :trail_categories
     has_many :users, through: :trails
+
+    def slug
+        name.downcase.tr(' ', '-')
+    end
+    
+    def self.find_by_slug(slug)
+        Category.all.find { |category| category.slug == slug }
+    end
 end
