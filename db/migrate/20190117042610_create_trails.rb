e5e39da53edd199fbd6fc89453bdee7901ade2a9
github username <github email address>
2019-01-24 16:49:26 -0500

    add memo section to trail params

--- db/migrate/20190117042610_create_trails.rb
+++ db/migrate/20190117042610_create_trails.rb
@@ -6,6 +6,7 @@ class CreateTrails < ActiveRecord::Migration
       t.float :length
       t.string :bathroom
       t.string :cell_reception
+      t.string :memo
       t.timestamps null: false
     end
   end
