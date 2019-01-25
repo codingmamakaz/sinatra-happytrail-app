a894c5fc9270ad1e20b123c014c92fd27754eaed
github username <github email address>
2019-01-25 10:28:13 -0500

    seed new data and fix css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+#Create two users
+kazumi = User.create(username: "Kazumi", email: "kazumi@gmail.com", password: "1234")	
+john = User.create(username: "John", email: "john@gmail.com", password: "1234")	
+
+#Create three trails
+
+panhandle = Trail.create(name:  "Pan handle", length: 1.5, bathroom: false, cell_reception: true, user_id: kazumi.id)	
+frick = Trail.create(name:  "Frick", length: 1, bathroom: true, cell_reception: true, user_id: kazumi.id)
+scrubgrass = Trail.create(name:  "Scrubgrass", length: 1.7, bathroom: false, cell_reception: true, user_id: john.id)
 
 #Create four categories
 
